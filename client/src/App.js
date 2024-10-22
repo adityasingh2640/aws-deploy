5d3d66ba20716d77473cef3a534da39643650497
Kushwaha <adityasinghk@kpmg.com>
2024-10-22 21:34:47 +0530

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import './App.css';
+import Axios from "axios";
+
+function App() {
+  const [url,setURL] = useState("");
+  useEffect(() => {
+    let axios = Axios.create({
+      baseURL: 'http://localhost:5000/api'
+    })
+    axios.post("/todo", { text: "Aditya", isDone: true }).then(resp => {
+      console.log(resp.data);
+      if(resp?.data?.text) setURL(resp.data.text)
+    })
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello <code>{url}</code>.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const { DB_CONN } = process.env;
+
+const api = express();
+api.use(cors()); // enable CORS on all our requests
+api.use(express.json()); // parses incoming requests with JSON payloads
+api.use(express.urlencoded({ extended: false })); // parses incoming requests with urlencoded payloads
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const databaseName = process.env.DB_NAME;
+const host = process.env.DB_HOSTNAME;
+
+// let connectionURI = `mongodb+srv://${username}:${password}@${host}/${databaseName}?retryWrites=true&w=majority`;
+// console.log(connectionURI)
+// mongoose.connect(connectionURI)
+//     .then(() => console.log("DB connection successful"))
+//     .catch(e => {
+//         console.log(e)
+//     });
+
+// const ToDoModel = mongoose.model("todo", new mongoose.Schema({
+//     isDone: Boolean,
+//     text: String,
+// })
+// );
+
+api.post("/api/todo", (req, res) => {
+    console.log(req.body)
+    let todo = req.body;
+    todo.text = todo.text + " Singh"
+    todo.date = Date.now();
+    res.status(200).send(todo);
+
+    // todo._id = new mongoose.Types.ObjectId(); // mongoose does not resolve automatically ids for new objects
+    // const newTodo = new ToDoModel(todo);
+
+    // newTodo.save().then((newTodo) => {
+    //     res.json(newTodo);
+    // })
+    //     .catch((err) => {
+    //         res.status(400).send(err);
+    //     });
+});
+
+api.use(express.static(path.join(__dirname, "client", "build")));
+api.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+const port = process.env.PORT || 5000;
+api.listen(port,()=>{
+    console.log('Server up !', port)
+});
